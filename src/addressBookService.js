7a5ac8d9ec60aab821f32dec68a0ca3aa76a4ddc
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-07-13 22:42:53 +0530

    added:find contact by name

--- src/addressBookService.js
+++ src/addressBookService.js
@@ -1,16 +1,32 @@
 /**
- * To create a
-New Address Book
-array and add new
-Contacts to it
+ * To find person by name from address book array
  */
 import {
     Contact
 } from './contact.js';
 var addressBookArr = [];
+
+/**
+ * Function to add contact to address book array
+ * @param {*} contact 
+ * @returns addressBookArr
+ */
 const addContactToAddressBook = (contact) => {
     addressBookArr.push(contact);
-    console.log(addressBookArr);
+    return addressBookArr;
+}
+
+/**
+ * Function to find contact by name
+ * @param {*} firstName 
+ * @returns result
+ */
+const findContactByName = (firstName) => {
+    var result = addressBookArr.filter(contact => contact.firstName === firstName);
+    return result;
 }
 let contact = new Contact("1", "Ananya", "Gowda", "agrahara", "nrpura", "chikmagalur", "9876547643", "122343", "something@something.com");
-addContactToAddressBook(contact);
+let contact1 = new Contact("2", "Aksha", "Nishanth", "shettykoppa", "koppa", "shimoga", "9976547643", "198343", "aksha@something.com");
+console.log(addContactToAddressBook(contact));
+console.log(addContactToAddressBook(contact1));
+console.log(findContactByName("Ananya"));
