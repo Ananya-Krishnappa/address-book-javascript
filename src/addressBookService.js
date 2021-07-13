dfc70e31685335cc10e7e5e8b3e576d532506558
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-07-13 23:05:34 +0530

    added:delete contact by name

--- src/addressBookService.js
+++ src/addressBookService.js
@@ -1,5 +1,5 @@
 /**
- * To find person by name from address book array
+ * To delete person by name from address book array
  */
 import {
     Contact
@@ -25,8 +25,22 @@ const findContactByName = (firstName) => {
     var result = addressBookArr.filter(contact => contact.firstName === firstName);
     return result;
 }
+
+/**
+ * Dunction to delete contact ny name
+ * @param {*} firstName 
+ * @returns addressBookArr
+ */
+const deleteContactByName = (firstName) => {
+    addressBookArr = addressBookArr.filter(contact => contact.firstName != firstName);
+    return addressBookArr;
+}
 let contact = new Contact("1", "Ananya", "Gowda", "agrahara", "nrpura", "chikmagalur", "9876547643", "122343", "something@something.com");
 let contact1 = new Contact("2", "Aksha", "Nishanth", "shettykoppa", "koppa", "shimoga", "9976547643", "198343", "aksha@something.com");
+let contact2 = new Contact("3", "Anik", "Prabath", "simse", "goolu", "moolu", "9906547643", "199343", "anik@something.com");
 console.log(addContactToAddressBook(contact));
 console.log(addContactToAddressBook(contact1));
+console.log(addContactToAddressBook(contact2));
 console.log(findContactByName("Ananya"));
+deleteContactByName("Aksha");
+console.log("Array after delete", addressBookArr);
